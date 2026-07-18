a1166e441422220b99a831002bd0ed8df09b78c8
janasilvamat <janainasilvamat@gmail.com>
2026-07-17 21:52:31 -0300

    Finalizado aula 1 dia 17072026

---
+++ Lógica de Programação e Algoritmos/Aula01/SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+int n;
+int numero = 12;
+double flutuante = 2.85;
+string palavra = "Senai";
+char caracter = 'S';
+bool logica = true;
+bool logica2 = false;
+
+WriteLine($"SENAI Americana {palavra}");
+WriteLine("Curso DEV BackEnd");
+Write("Argentina"); //Vai perder
+ReadKey();
