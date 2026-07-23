527c2fd101b57cbcccef12999814194514d913fd
janasilvamat <janainasilvamat@gmail.com>
2026-07-22 21:56:54 -0300

    Medidas, try, catch, finally

---
+++ Lógica de Programação e Algoritmos/Aula03/Operadores/Medidas/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+
+try 
+{
+    //Declaração de variáveis
+    int a, b, c;
+    double area_q, area_t, area_tp;
+
+    //Entrada de dados
+    Write("Digite o valor de A: ");
+    a = int.Parse(ReadLine());
+    Write("Digite o valor de B: ");
+    b = int.Parse(ReadLine());
+    Write("Digite o valor de C: ");
+    c = int.Parse(ReadLine());
+
+    //Processamento de dados
+    area_q = Math.Pow(a, 2);
+    area_t = (a * b) / 2;
+    area_tp = ((a + b) * c) / 2;
+
+    //Saída de dados
+    WriteLine($"Área do quadrado = {area_q:f4}");
+    WriteLine($"Área do triângulo = {area_t:f4}");
+    WriteLine($"Área do trapézio = {area_tp:f4}");
+}
+catch //Tratamento de exceções
+{
+    WriteLine("Digite um valor válido");
+}
+finally 
+{
+    WriteLine("Encerrando programa...");
+
+}
+
+
+    ReadKey();
+
+
