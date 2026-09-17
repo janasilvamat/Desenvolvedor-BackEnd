618722133dba8c382768577cd6c1349b2fc54478
janasilvamat <janainasilvamat@gmail.com>
2026-09-16 21:56:34 -0300

    Inserção e consulta de dados no BD.

---
+++ Banco de Dados/Aula 06/Cars/Cars/Classes/Contextos/EstoqueContexto.cs
@@ -0,0 +1,39 @@
+﻿
+using Cars.Classes.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cars.Classes.Contextos
+{
+    internal class EstoqueContexto : DbContext
+    {
+        //Propriedade
+        public DbSet<Estoque> Estoque { get; set; }
+
+        //Métodos
+
+        //Métodos
+        protected override void OnConfiguring(DbContextOptionsBuilder opcoesDeConstrucao)
+        {
+            string caminho = @"Server=ECFP507D1319388\SQLEXPRESS03;Database=Automoveis;Trusted_Connection=True;TrustServerCertificate=True";
+            opcoesDeConstrucao.UseSqlServer(caminho);
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modeloDeConstrucao)
+        {
+            modeloDeConstrucao.Entity<Estoque>(entidade =>
+            {
+                entidade.HasKey(e => e.Id);
+                entidade.Property(e => e.Modelo);
+                entidade.Property(e => e.Tipo);
+                entidade.Property(e => e.Valor);
+                entidade.Property(e => e.Quantidade);
+                entidade.Property(e => e.Foto);
+            }
+
+        );
+        }
+
+
+    }
+}
---
+++ Banco de Dados/Aula 06/Cars/Cars/Classes/Services/Pesquisa.cs
@@ -0,0 +1,69 @@
+﻿
+
+using Cars.Classes.Contextos;
+using Microsoft.IdentityModel.Tokens;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Cars.Classes.Services
+{
+    internal static class Pesquisa
+    {
+        //Campo
+        static EstoqueContexto contexto = new EstoqueContexto();
+
+        //Métodos
+        public static void Pesquisas(bool moto, bool carro, string texto, DataGridView dataGridView1)
+        {
+
+            if (moto.Equals(false) & carro.Equals(false) | moto.Equals(true) & carro.Equals(true))
+            {
+                Filtro(texto, dataGridView1);
+            }
+
+            else if (moto)
+            {
+                dataGridView1.DataSource = contexto.Estoque.Where(e => e.Tipo == 1).Select(e => new
+                {
+                    e.Tipo,
+                    e.Modelo,
+                    e.Quantidade,
+                    Valor = e.Valor.ToString("C")
+                }).ToList();
+            }
+            else if (carro)
+            {
+                dataGridView1.DataSource = contexto.Estoque.Where(e => e.Tipo == 2).Select(e => new
+                {
+                    e.Tipo,
+                    e.Modelo,
+                    e.Quantidade,
+                    Valor = e.Valor.ToString("C")
+                }).ToList();
+            }
+        }
+        private static void Filtro (string texto, DataGridView dataGridView1)
+        {
+            if (texto.IsNullOrEmpty())
+            {
+                dataGridView1.DataSource = contexto.Estoque.Select(e => new
+                {
+                    e.Tipo,
+                    e.Modelo,
+                    e.Quantidade,
+                    Valor = e.Valor.ToString("C")
+                }).ToList();
+            }
+            else
+            {
+                dataGridView1.DataSource = contexto.Estoque.Where(e => e.Modelo.Contains(texto)).Select(e => new
+                {
+                    e.Tipo,
+                    e.Modelo,
+                    e.Quantidade,
+                    Valor = e.Valor.ToString("C")
+                }).ToList();
+            }
+        }
+    }
+}   
+
